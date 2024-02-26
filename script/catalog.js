1e67e561f1a847cb597a9744ab83bd8a36bae015
violetajosic <violetajosicc0@gmail.com>
2024-02-26 23:45:52 +0100

    Dodata mnc -make new catalog strana

--- script/catalog.js
+++ script/catalog.js
@@ -1,16 +1,1 @@
-var exitBtn = document.querySelector(".exitBtn");
-var exitBtnHover = document.querySelector(".exitBtnHover");
-// Add a click event listener - menjanje slike dugmeta na hover
-if (exitBtn !== null && exitBtnHover !== null) {
-
-    exitBtn.addEventListener("mouseover", function (event) {
-    exitBtn.style.display = "none";
-    exitBtnHover.style.display = "flex";
-    });
-
-    exitBtn.addEventListener("mouseout", function (event) {
-    exitBtnHover.style.display = "none";
-    exitBtn.style.display = "flex";
-    });
-
-}
+var vetInputTable = document.querySelector(".vetInputTable").style.display="flex";
---
+++ script/exitBtn.js
@@ -0,0 +1,16 @@
+var exitBtn = document.querySelector(".exitBtn");
+var exitBtnHover = document.querySelector(".exitBtnHover");
+// Add a click event listener - menjanje slike dugmeta na hover
+if (exitBtn !== null && exitBtnHover !== null) {
+
+    exitBtn.addEventListener("mouseover", function (event) {
+    exitBtn.style.display = "none";
+    exitBtnHover.style.display = "flex";
+    });
+
+    exitBtn.addEventListener("mouseout", function (event) {
+    exitBtnHover.style.display = "none";
+    exitBtn.style.display = "flex";
+    });
+
+}
