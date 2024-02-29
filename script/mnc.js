a358401d78cedc7cfe2753dea46acd0c7144b227
violetajosic <violetajosicc0@gmail.com>
2024-02-29 12:43:01 +0100

    Dodato racunanje zivotinjskih godina u ljudske

--- script/mnc.js
+++ script/mnc.js
@@ -1,3 +1,4 @@
+//upload image
 function displaySelectedImage(event, elementId) {
     const selectedImage = document.getElementById(elementId);
     const fileInput = event.target;
@@ -12,3 +13,29 @@ function displaySelectedImage(event, elementId) {
         reader.readAsDataURL(fileInput.files[0]);
     }
 }
+
+//count age
+//count age
+var petInput = document.querySelector(".petInput"); // uzima se referenca na polje za unos godina
+var mncConverted = document.querySelector(".mncConverted"); // ovde će se prikazivati izračunato
+var speciesInput = document.querySelector(".speciesInput").value; // input vrste
+
+petInput.addEventListener("input", function() {
+    var petAge = parseInt(petInput.value); // konvertuje se vrednost unosa u broj
+
+    if (speciesInput !== "cat") {
+        if (petAge < 2) {
+            mncConverted.textContent = petAge * 10.5;
+        } else if (petAge >= 2) {
+            mncConverted.textContent = 2 * 10.5 + (petAge - 2) * 4;
+        }
+    } else {
+        mncConverted.textContent = 15 + 9 * (petAge - 1); //drugacije racunanje za macke
+    }
+});
+
+// Dodajte dodatnu proveru za "speciesInput" kada se vrednost promeni
+document.querySelector(".speciesInput").addEventListener("input", function() {
+    speciesInput = document.querySelector(".speciesInput").value;
+    mncConverted.textContent = "0"; // Resetujte prikaz prilikom promene vrste
+});
