e2f093d7671961165166eb0fe0152b301758b7ac
violetajosic <violetajosicc0@gmail.com>
2024-05-09 21:26:54 +0200

    popravljen profile membership js da sakrije log out dugme

--- script/profileMem.js
+++ script/profileMem.js
@@ -2,8 +2,7 @@ function profileMembership (){
     document.querySelector(".profileHeadingRow").style.display="none";
     document.querySelector(".profileRow").style.display="none";
     document.querySelector(".profileButton").style.display="none";
-    var logBtn = document.querySelector("button.profileClientBtn"); 
-    logBtn.style.display="none !important";//ne radi da sakrije log out dugme
+    document.querySelector(".row-12.profileButton.logOutButton.profileLogOutBtn").style.setProperty("display", "none", "important");
     document.querySelector(".profileMembershipAfter").style.display="flex";
     document.querySelector(".profileAfterBtn").style.display="flex";
     document.querySelector(".profileAfterBtnPay").style.display="flex";
