502783eb751d35f60e580309f489cd447e071f55
violetajosic <violetajosicc0@gmail.com>
2024-05-09 14:30:17 +0200

    izmenjen catalog.js catalog.html i catalog.php tako da je povezano sa bazom

--- catalog.php
+++ catalog.php
@@ -4,24 +4,22 @@ $username = "root";
 $password = "";
 $database = "addApointment";
 
-// Create connection
+//konekcija
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
+//provera konekcije
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve input values
-$diseaseName = $_POST['diseaseName'];
-$appointmentDate = $_POST['appointmentDate'];
-$symptoms = $_POST['symptoms'];
-$therapy = $_POST['therapy'];
-$doctorInfo = $_POST['doctorInfo'];
-$clinicIDInfo = $_POST['clinicIDInfo'];
+$diseaseName = $_POST['catalogDiseaseName'];
+$appointmentDate = $_POST['catolgDate'];
+$symptoms = $_POST['catalogSympt'];
+$therapy = $_POST['catalogTherapy'];
+$doctorInfo = $_POST['catalogDoctor'];
+$clinicIDInfo = $_POST['catalogClinicID'];
 
-// Insert data into the appointments table
-$sql = "INSERT INTO appointments (disease_name, appointment_date, symptoms, therapy, doctor_info, clinicIDInfo)
+$sql = "INSERT INTO appointments (disease_name, appointment_date, symptoms, therapy, doctor_info, clinic_id)
         VALUES ('$diseaseName', '$appointmentDate', '$symptoms', '$therapy', '$doctorInfo', '$clinicIDInfo')";
 
 if ($conn->query($sql) === TRUE) {
